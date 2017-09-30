ecd315314168305b2778e54ee88e37bb2c2508be
Ron Danon <rondanon@gmail.com>
2017-09-30 14:56:30 +0300

    add calculate RMSE method to tool.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "tools.h"
+#define RMSE_SIZE 4
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
@@ -15,4 +16,23 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(RMSE_SIZE);
+  VectorXd error(RMSE_SIZE);
+  rmse.setZero();
+
+  for (int i = 0; i < estimations.size(); ++i){
+    // transform the estimations state to the ground truth state (px, py, vx, vy)
+    error[0] = estimations[i][0];
+    error[1] = estimations[i][1];
+    error[2] = estimations[i][2] * cos(estimations[i][3]);
+    error[3] = estimations[i][2] * sin(estimations[i][3]);
+
+    //compute error
+    error -= ground_truth[i];
+    rmse += error.array()*error.array();
+  }
+  //compute rmse
+  rmse /= estimations.size();
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
